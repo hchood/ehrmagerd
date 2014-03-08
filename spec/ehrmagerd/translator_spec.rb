99d5d5757e42d6fe0fc9fe967af8abfe725768c5
Helen Hood <helen.c.hood@gmail.com>
2014-03-08 10:31:13 -0500

    Add 2nd test, refactor first one

--- spec/ehrmagerd/translator_spec.rb
+++ spec/ehrmagerd/translator_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 
 describe Ehrmagerd::Translator do
   it 'translates a string into ehrmagerd-speak' do
-    test_string = "Oh my god"
-    expect(Ehrmagerd::Translator.translate(test_string)).to eq "Ehrmagerd"
+    test_string1 = "Oh my god"
+    expect(Ehrmagerd::Translator.translate(test_string1)).to eq "EHRMAGERD"
+
+    test_string2 = "Dogecoins are dumb"
+    expect(Ehrmagerd::Translator.translate(test_string1)).to eq "DERGERCERNS ER DERMB"
   end
 end
