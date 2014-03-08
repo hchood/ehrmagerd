7d59c9566e60fee630b7aca885fcebe0a0d2a7b1
Helen Hood <helen.c.hood@gmail.com>
2014-03-08 12:08:20 -0500

    Use VCR for tests

--- spec/ehrmagerd/translator_spec.rb
+++ spec/ehrmagerd/translator_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Ehrmagerd::Translator do
+describe Ehrmagerd::Translator, :vcr do
   it 'translates a string into ehrmagerd-speak' do
     test_string1 = "OhMyGod Goosebumps my favorite books!"
     expect(Ehrmagerd::Translator.translate(test_string1)).to eq "ERMAHGERD GERSBERMS MAH FRAVRIT BERKS!"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,17 @@ require 'rspec'
 require_relative '../lib/ehrmagerd'
 
 Ehrmagerd.api_token = ENV['API_TOKEN']
+
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.default_cassette_options = { :record => :new_episodes }
+  c.filter_sensitive_data('<EHRMAGERD API TOKEN>') { Ehrmagerd.api_token }
+end
+
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+end
