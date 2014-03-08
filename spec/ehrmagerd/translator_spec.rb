749af38a95ed4a5a3fba7785a07ad0a9e48c4b3a
Helen Hood <helen.c.hood@gmail.com>
2014-03-08 10:54:47 -0500

    Update spec to use the test post request's string

--- spec/ehrmagerd/translator_spec.rb
+++ spec/ehrmagerd/translator_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Ehrmagerd::Translator do
   it 'translates a string into ehrmagerd-speak' do
-    test_string1 = "Oh my god"
-    expect(Ehrmagerd::Translator.translate(test_string1)).to eq "EHRMAGERD"
+    test_string1 = "OhMyGod, Goosebumps, my favorite books!"
+    expect(Ehrmagerd::Translator.translate(test_string1)).to eq "ERMAHGERDGERSERBERMPSMAH FRAVRIT BERKS!"
 
     test_string2 = "Dogecoins are dumb"
     expect(Ehrmagerd::Translator.translate(test_string1)).to eq "DERGERCERNS ER DERMB"
