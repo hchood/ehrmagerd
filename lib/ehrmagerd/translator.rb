e7a7ee0e9904aa313d3ddeb728efc5639d22b335
Helen Hood <helen.c.hood@gmail.com>
2014-03-08 10:38:21 -0500

    Add post request into Translator.translate method; add unirest dependency

--- lib/ehrmagerd/translator.rb
+++ lib/ehrmagerd/translator.rb
@@ -1,9 +1,16 @@
 require "ehrmagerd/version"
+require "unirest"
 
 module Ehrmagerd
   class Translator
     def self.translate(string)
-      "Ehrmagerd"
+      response = Unirest::post "https://jmillerdesign-ermahgerd-translator.p.mashape.com/api",
+        headers: {
+          "X-Mashape-Authorization" => "hm4geMmQl70fwzsJAY9ddGVg9QtReNlj"
+        },
+        parameters: {
+          "input" => "[\"OhMyGod\",\"Goosebumps\",\"my favorite books!\"]"
+        }
     end
   end
 end
